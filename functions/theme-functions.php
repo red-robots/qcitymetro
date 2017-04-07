0d93582cc40beddf3709906ef782549c5f3c3be0
Fritz <fre.healy@gmail.com>
2017-04-07 15:30:47 -0400

    adding instructions to content and changing labels on front end form

--- functions/theme-functions.php
+++ functions/theme-functions.php
@@ -162,6 +162,17 @@ if( is_page(219) ) {
 	}
 //}
 }
+
+if( is_page(21) ) {
+    if($field['name'] == '_post_content') {
+        $field['label'] = 'Tell us about your business';
+	    $field['instructions'] = 'What products or services do your offer? What makes your business special? No more than 150 words.';
+    }
+	if($field['name'] == '_post_title') {
+		$field['label'] = 'Name of Business';
+	}
+//}
+}
 	//if($field['name'] == '_post_content') {
 		//$field['label'] = 'Custom Content Title';
 	//}
