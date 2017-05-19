23a5746127b5b70ca6cc1848a6db59cb83ef25f6
Fritz <fre.healy@gmail.com>
2017-05-19 13:06:32 -0400

    making redirect to sponsors page for tiers now

--- taxonomy-sponsor_category.php
+++ taxonomy-sponsor_category.php
@@ -1,4 +1,9 @@
 <?php 
-	wp_redirect( "/sponsors" );
+	$permalink = get_the_permalink(39809);
+	if($permalink):
+		wp_redirect( $permalink );
+	else:
+		wp_redirect(get_bloginfo('url'));
+	endif;
 	exit;
 ?>
