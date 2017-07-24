fbc2d6c5f2f96e3cce43faaad39daf524d471f9b
Fritz <fre.healy@gmail.com>
2017-07-24 13:22:05 -0400

    changing hook location

--- functions/theme-functions.php
+++ functions/theme-functions.php
@@ -486,7 +486,7 @@ function bella_register_buttons( $buttons ) {
     array_push( $buttons, 'signup' ); // dropcap', 'recentposts
     return $buttons;
 }
-add_action('init','bella_hide_publish_events');
+add_action('wp','bella_hide_publish_events');
 function bella_hide_publish_events(){
 	global $post;
 	if(strcmp(get_post_type($post),"event")===0){
