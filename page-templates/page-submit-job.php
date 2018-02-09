8bff7736b99c958612134d28a5b1471c46ba7db6
Fritz <fre.healy@gmail.com>
2018-02-09 13:50:01 -0500

    adding statistics

--- page-templates/page-submit-job.php
+++ page-templates/page-submit-job.php
@@ -18,47 +18,16 @@ get_header(); ?>
 					<?php echo $banner_copy;?>
 				</div><!--.row-1-->
 			<?php endif;?>
-			<div class="row-2">
-				<a href="<?php echo get_permalink(46635);?>">Post a Job</a>
-				<a href="<?php echo get_permalink();?>">Find a Job</a>
-			</div><!--.row-1-->
-			<div class="row-3">
-				<form action="<?php echo get_permalink(46657);?>" method="GET">
-					<div class="row-1">
-						<input type="text" name="search" placeholder="Search">
-						<button type="submit">
-							<i class="fa fa-search"></i>
-						</button>
-						<div class="clear"></div>
-					</div><!--.row-1-->
-					<div class="row-2">
-						<?php $terms = get_field("categories_to_show",46657);
-						if(is_array($terms)&&!empty($terms)):?>
-							<ul>
-								<li>Popular categories:</li>
-								<?php foreach($terms as $term):?>
-									<li>
-										<input type="radio" name="category" id="<?php echo $term->slug;?>" value="<?php echo $term->term_id;?>"><label for="<?php echo $term->slug;?>"><?php echo $term->name;?></label>
-									</li>
-								<?php endforeach;?>
-							</ul>
-						<?php endif;?>
-					</div><!--.row-2-->
-					<div class="row-3">
-						<?php $terms = get_terms(array('taxonomy'=>'level','hide_empty'=>false));
-						if(!is_wp_error($terms)&&is_array($terms)&&!empty($terms)):?>
-							<ul>
-								<li>level:</li>
-								<?php foreach($terms as $term):?>
-									<li>
-										<input type="radio" name="level" id="<?php echo $term->slug;?>" value="<?php echo $term->term_id;?>"><label for="<?php echo $term->slug;?>"><?php echo $term->name;?></label>
-									</li>
-								<?php endforeach;?>
-							</ul>
-						<?php endif;?>
-					</div><!--.row-3-->
-				</form>
-			</div><!--.row-1-->
+			<?php $statistics = get_field("statistics",46657);
+			if($statistics):?>
+				<div class="statistics">
+					<ul>
+						<?php foreach($statistics as $stat):?>
+							<li><?php echo $stat['copy'];?></li>
+						<?php endforeach;?>
+					</ul>
+				</div><!--.statistics-->
+			<?php endif;?>
 		</div><!--.jobs-banner-->
 		<div id="content" role="main" class="wrapper">
 
