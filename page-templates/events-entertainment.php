e49ec8e415ab28121bb73ff3c4cdd28dbe6a080d
fritzhealy <fre.healy@gmail.com>
2018-04-18 11:09:59 -0400

    functionality of events page complete

--- page-templates/events-entertainment.php
+++ page-templates/events-entertainment.php
@@ -3,314 +3,183 @@
  * Template Name: Events & Entertainment
  */
 
-get_header(); 
+get_header();?>
+    <?php if ( have_posts() ) : the_post(); ?>
+        <div id="primary" class="">
+            <?php $banner_image = get_field("banner_image");
+            $banner_copy = get_field("banner_copy");?>
+            <div class="jobs-banner event-banner">
+                <?php if($banner_image) echo '<div class="background" style="background-image: url('.$banner_image['url'].');"></div>';?>
+                <?php if($banner_copy):?>
+                    <div class="row-1">
+                        <?php echo $banner_copy;?>
+                    </div><!--.row-1-->
+                <?php endif;?>
+                <div class="row-2">
+                    <a class="banner-button" href="<?php echo get_permalink(48786);?>">Post an Event</a>
+                    <div class="banner-button find">Find an Event</div>
+                </div><!--.row-1-->
+                <div class="row-3">
+                    <form action="<?php echo get_permalink();?>" method="GET">
+                        <div class="row-1">
+                            <input type="text" name="search" placeholder="Search">
+                            <button type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                            <div class="clear"></div>
+                        </div><!--.row-1-->
+                        <div class="row-2">
+                            <?php $terms = get_field("categories_to_show");
+                            if(is_array($terms)&&!empty($terms)):?>
+                                <ul>
+                                    <li>categories:</li>
+                                    <?php foreach($terms as $term):?>
+                                        <li>
+                                            <input type="radio" name="category" id="<?php echo $term->slug;?>" value="<?php echo $term->term_id;?>"><label for="<?php echo $term->slug;?>"><?php echo $term->name;?></label>
+                                        </li>
+                                    <?php endforeach;?>
+                                </ul>
+                            <?php endif;?>
+                        </div><!--.row-2-->
+                        <div class="row-3">
+                            <ul>
+                                <li>date:</li>
+                                <li>
+                                    <input type="radio" name="date" id="date-today" value="today"><label for="date-today">today</label>
+                                </li>
+                                <li>
+                                    <input type="radio" name="date" id="date-week" value="week"><label for="date-week">this week</label>
+                                </li>
+                                <li>
+                                    <input type="radio" name="date" id="date-month" value="month"><label for="date-month">this month</label>
+                                </li>
+                                <li>
+                                    <input type="radio" name="date" id="date-year" value="year"><label for="date-year">this year</label>
+                                </li>
+                            </ul>
+                        </div><!--.row-3-->
+                    </form>
+                </div><!--.row-1-->
+            </div><!--.event-banner-->
+            <div id="content" role="main" class="wrapper">
 
-?>
+                <div class="site-content">
 
-<?php get_template_part('inc/events'); ?>
-<?php 
- ?>
- <script>
-    $(function(){
-      // bind change event to select
-      $('#eventtype').on('change', function () {
-          var url = $(this).val(); // get selected value
-          if (url) { // require a URL
-              window.location = url; // redirect
-          }
-          return false;
-      });
-    });
-	
-</script>
-	<div id="primary" class="">
-		<div id="content" role="main" class="wrapper">
+                    <?php // for query of today and forward
+                    $today = date('Ymd');
+                    $future = null;
+                    if(isset($_GET['date'])&&!empty($_GET['date'])):
+                        $add = null;
+                        if(strcmp($_GET['date'],'today')==0):
+                            $add = 'P1D';
+                        elseif(strcmp($_GET['date'],'week')==0):
+                            $add = 'P7D';
+                        elseif(strcmp($_GET['date'],'month')==0):
+                            $add = 'P1M';
+                        elseif(strcmp($_GET['date'],'year')==0):
+                            $add = 'P1Y';
+                        endif;
+                        if($add!==null):
+                            $enddate = new DateTime('NOW');
+                            $enddate->add(new DateInterval($add));
+                            $future = $enddate->format('Ymd');
+                        endif;//if add not null
+                    endif;//if for date set
+                    
+                    $args = array(
+                        'post_type'=>'event',
+                        'posts_per_page' => -1,
+                        'orderby'=>'meta_value',
+                        'meta_key'=>'event_date',
+                        'order'=>'ASC'
+                    );
+                    $meta = array();
+                    $meta_cat = null;
+                    $meta_date = null;
+                    if($future!==null):
+                        $meta_date = array(
+                            'relation'=>'AND',
+                            array(
+                                'key'=>'event_date',
+                                'value'=>$today,
+                                'compare'=>'>=',
+                                'type'=>'NUMERIC'
+                            ),
+                            array(
+                                'key'=>'event_date',
+                                'value'=>$future,
+                                'compare'=>'<',
+                                'type'=>'NUMERIC'
+                            )
+                        );
+                    else: 
+                        $meta_date = array(
+                            'relation'=>'OR',
+                            array(
+                                'key'=>'event_date',
+                                'value'=>$today,
+                                'compare'=>'>=',
+                                'type'=>'NUMERIC'
+                            ),
+                            array(
+                                'key'=>'event_date',
+                                'compare' => 'NOT EXISTS'
+                            )
+                        );
+                    endif;
+                    if(isset($_GET['search'])&&!empty($_GET['search'])):
+                        $prepare_string = "SELECT ID FROM $wpdb->posts WHERE post_title LIKE '%%%s%%' AND post_type = 'event' ";
+                        $prepare_string .= "UNION SELECT object_id FROM $wpdb->term_relationships as r INNER JOIN $wpdb->terms as t ON t.term_id = r.term_taxonomy_id WHERE t.name LIKE '%%%s%%'";
+                        $prepare_args[] = $_GET['search'];
+                        array_unshift($prepare_args,$_GET['search']);
+                        array_unshift($prepare_args,$prepare_string);
+                        $results = $wpdb->get_results(  call_user_func_array(array($wpdb, "prepare"),$prepare_args));
+                        $in = array();
+                        if($results):
+                            foreach($results as $result):
+                                $in[] = $result->ID;
+                            endforeach;
+                        else:
+                            $in[] = -1;
+                        endif;
+                        $args['post__in']= $in;
+                    endif;
+                    if(isset($_GET['category'])&&!empty($_GET['category'])):
+                        $meta_cat = array(
+                            'key'     => 'category',
+                            'value'   => '"'.$_GET['category'].'"',
+                            'compare' => 'LIKE'
+                        );
+                    endif;
+                    $meta[] = $meta_date;
+                    if(!empty($meta_cat)):
+                        $meta[] = $meta_cat;
+                    endif;
+                    $args['meta_query'] = $meta;
+                    var_dump($args);
+                    $query = new WP_Query($args);
+                    if ($query->have_posts()) : 
+                        while ($query->have_posts()) :  $query->the_post(); ?>
+                            <h1><?php the_title();?></h1>
+                            <h2><?php echo get_field('event_date');?></h2> 
+                        <?php endwhile;
+                        wp_reset_postdata();
+                    endif;?> 
+                </div><!-- site content -->
+                
+                <!-- 
+                            Ad Zone
 
-<?php while ( have_posts() ) : the_post(); ?>
-
-<div class="site-content">
-
-    <header class="archive-header">
-        <div class="border-title">
-        	<h1><?php the_title(); ?></h1>
-        </div><!-- border title -->
-    </header><!-- .archive-header -->
-
-
-
-<div class="event-page-links">
-
-    <div class="button button-thirds button-thirds-first">
-    	<a href="<?php bloginfo('url'); ?>/submit-an-event">Submit an event</a>
-    </div>
-    <!--<div class="button button-thirds button-thirds-first">
-    	<a href="<?php bloginfo('url'); ?>/submit-an-event">promote your event</a>
-    </div>-->
-    <div class="button button-thirds button-thirds-last">
-    	<a href="<?php bloginfo('url'); ?>/event-list">view all events</a>
-    </div>
-
-<?php 
-$url = get_bloginfo('url');
-$cDir = $url . '/event-list';
-//echo $url;
-
-$denomargs = array(
-    'orderby'           => 'name', 
-    'order'             => 'ASC',
-    'hide_empty'        => true, 
-    'fields'            => 'all', 
-); 
-
-$dterms = get_terms('event_cat', $denomargs);
-
-/*echo '<pre>';
-print_r($dterms);
-echo '<pre>';*/
-?>
-<form id="category-select" class="button button-thirds button-thirds-first"  action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
-
-        <select name='eventtype' id='eventtype' class="eventtype"  >
-        	<?php 
-			echo '<option class="">View by Event Type</option>';
-			echo '<option class="" value="' . $cDir . '">All</option>';
-			foreach( $dterms as $dterm ) :
-				echo '<option class="level-0" value="'.$url.'/event-category/'.$dterm->slug.'">'.$dterm->name.'</option>';
-			endforeach;
-			
-			?>
-        </select>
-
-		<noscript>
-			<input type="submit" value="View" />
-		</noscript>
-</form>
-
-
-
-<?php endwhile; // end of the loop. ?>
-
-
-</div><!-- grey box -->
-
-<?php 
-// for query of today and forward
-$today = date('Ymd');
-$i = 0;
-// Set the date to break out of loop at the bottom
-$enddate = new DateTime(date("Ymd"));
-$enddate->modify('+10 day');
-$stop = $enddate->format('Ymd');
-/*
-
-	Query for the Next 10 days
-		
----------------------------------
-*/
-// Start query
-	$wp_query = new WP_Query();
-    $wp_query->query(array(
-    'post_type'=>'event',
-    'posts_per_page' => -1,
-	'meta_key' => 'event_date',
-    'meta_value' => $today,
-    'meta_compare' => '>='
-));
-    if ($wp_query->have_posts()) : 
-	while ($wp_query->have_posts()) :  $wp_query->the_post(); 
-	
-	 // our Event variables
-	 $title = get_the_title();
-	 $permalink = get_the_permalink();
-	 $image = get_field('event_image'); 
-	 $location = get_field('venue_address');
-	 $start = get_field('event_start_time');
-	 $cost = get_field('cost_of_event');
-   $venueName = get_field('name_of_venue');
-	 $postId = get_the_ID();
-	 $culture_block = get_field("culture_block");
-	 $charlotte_works_block = get_field("charlotte_works_block");
-	 $terms = wp_get_post_terms( $postId, 'event_category' );
-	 $date = DateTime::createFromFormat('Ymd', get_field('event_date')); 
-	 $eDate = $date->format('Ymd');
-	 // create the array
-	 
-	 $mySort = array (
-	 	'date' => $eDate,
-		'title' => $title,
-		'permalink' => $permalink,
-		'location' => $location,
-		'time' => $start,
-		'cost' => $cost,
-		'image' => $image,
-		'terms' => $terms,
-        'venue' => $venueName,
-        'culture'=> $culture_block,
-        'cw'=>$charlotte_works_block
-	 );
-	 
-	 // put in new array
-	 $newQuery[] = $mySort;
-	 
-	 endwhile; 
-	endif; // end loop query
-
-/*
-		Comparison function for the sort
-	
-===========================================*/	
-function cmp($a, $b) {
-   $result = 0;
-   
-   if ( $a['date'] == $b['date'] ) {
-      // Dates are same so compare names within the date.
-      if(!empty($a['terms'])&&!empty($b['terms'])){
-        if ( $a['terms']['0']->slug['0'] < $b['terms']['0']->slug['0'] )
-            $result = -1;
-        else
-            if ( $a['terms']['0']->slug['0'] > $b['terms']['0']->slug['0'] )
-                $result = 1;
-      }
-   }
-   else {
-      // Dates differ so just compare on date.
-      if ( $a['date'] < $b['date'] )
-         $result = -1;
-      else
-         $result = 1;
-   }
-   return $result;
-}
-
-if(isset($newQuery)):
-    // sort the Query
-    usort($newQuery,'cmp');
-
-    $prevDay = '';
-
-        foreach ($newQuery as $value) : 
-        // get the term
-        $currentTerm = !empty($value['terms']) ? $value['terms']['0']->slug : '';
-        // set the date
-        $getDate = $value['date'];
-        $newD = DateTime::createFromFormat('Ymd', $getDate);
-        $day = $newD->format('l, F j, Y');
-    $daynum = $newD->format('n/j/y');
-        // set image
-        $image = $value['image']['sizes']['thumbnail'];
-        // Fill in the day
-        if( $getDate != $prevDay ) {
-        ?>
-        <div class="event-page-date js-first-word"><?php echo $day; ?></div>
-        <?php 
-        $prevDay = $getDate;
-        } // if month is not empty
-        ?>
+                ======================================================== -->        
+                <div class="widget-area">
+                    <?php 
+                    //get_template_part('ads/right-big'); 
+                    //get_template_part('ads/right-small');
+                   // get_template_part('ads/right-rail');
+                    ?>
+                </div><!-- widget area -->
         
-        <div class="featured-event">
-            <?php if(strcmp($value['culture'],"yes")===0):?>
-                <div class="culture">
-                    <div class="circle">
-                        ?
-                    </div><!--.circle-->
-                    <a href="https://www.artsandscience.org/programs/for-community/culture-blocks/asc-culture-blocks-upcoming-events/" target="_blank">
-                        <img src="<?php echo get_template_directory_uri()."/images/culture-blocks-title.jpg";?>" alt="Culture Blocks">
-                    </a>
-                    <?php $desc = get_field("culture_block_rollover",54);
-                    if($desc):?>
-                        <div class="rollover">
-                            <?php echo $desc;?>	
-                        </div><!--.rollover-->
-                    <?php endif;?>
-                </div><!--.culture-->
-                <div class="clear"></div>
-            <?php elseif(strcmp($value['cw'],'yes')===0):?>
-                <div class="culture cw">
-                    <div class="circle">
-                        ?
-                    </div><!--.circle-->
-                    <a href="https://www.artsandscience.org/programs/for-community/culture-blocks/asc-culture-blocks-upcoming-events/" target="_blank">
-                        <img src="<?php echo get_template_directory_uri()."/images/charlotte-works-logo.jpg";?>" alt="Charlotte Works">
-                    </a>
-                    <?php $desc = get_field("charlotte_works_block_rollover",54);
-                    if($desc):?>
-                        <div class="rollover">
-                            <?php echo $desc;?>	
-                        </div><!--.rollover-->
-                    <?php endif;?>
-                </div><!--.culture-->
-                <div class="clear"></div>
-            <?php endif;?>
-        <div class="featured-event-content-details">
-            <a href="<?php echo $value['permalink']; ?>">DETAILS</a>
-
-        <div class="featured-event-content-details-text">DETAILS ></div><!-- featured event content -->
-        
-        <div class="featured-event-content">
-                <h2><?php echo $value['title']; ?></h2>
-            <!-- <div class="el-date"><?php echo $daynum; ?></div> -->
-            <div class="el-deets">Venue</div>
-            <div class="fe-start"><?php echo $value['venue']; ?></div>
-            <!--  <div class="el-deets">Address</div>
-                <div class="fe-location"><?php echo $value['location']; ?></div> -->
-                <div class="el-deets">Time</div>
-                <div class="fe-start"><?php echo $value['time']; ?></div>
-                <div class="el-deets">Cost</div>
-                <div class="fe-cost"><?php echo $value['cost']; ?></div>
-
-            </div><!-- featured event content -->
-
-            <div class="featured-event-image">
-                <?php if( $image != '' ) { ?>
-                        <img src="<?php echo $image; ?>" />
-                <?php } ?>
-            </div><!-- featured event image -->
-
-            <div class="daynum">
-            <?php echo $daynum; ?>
-            </div>
-            <div class="clear"></div>
-        </div><!-- featured event content -->
-
-        </div><!-- featured event -->
-        
-    <?php
-    // Only go out 10 days..., then get out.
-    if( $value['date'] >= $stop ) {break;}
-    // end resorted loop
-    endforeach;
-endif;   ?>
-
-
-    <div class="button button-thirds button-thirds-first">
-    	<a href="<?php bloginfo('url'); ?>/submit-an-event">Submit an event</a>
-    </div>
-    <div class="button button-thirds button-thirds-first">
-    	<a href="<?php bloginfo('url'); ?>/submit-an-event">promote your event</a>
-    </div>
-    <div class="button button-thirds button-thirds-last">
-    	<a href="<?php bloginfo('url'); ?>/event-list">view all events</a>
-    </div> 
-
-
-
-
-            
-</div><!-- site content -->
-            
-<!-- 
-			Ad Zone
-
-======================================================== -->        
-        <div class="widget-area">
-        	<?php 
-			get_template_part('ads/right-big'); 
-			get_template_part('ads/right-small');
-			get_template_part('ads/right-rail');
-			?>
-        </div><!-- widget area -->
-        
-        </div><!-- #content -->
-	</div><!-- #primary -->
-
+            </div><!-- #content -->
+        </div><!-- #primary -->
+    <?php endif; //main loop?>
 <?php get_footer(); ?>
