f6e08420a681136a93fba43b3ea6dd13a9a2b6c5
fritzhealy <fre.healy@gmail.com>
2018-05-14 11:07:15 -0400

    changing ver on custom js

--- functions/scripts.php
+++ functions/scripts.php
@@ -50,7 +50,7 @@
 		wp_enqueue_script(
 			'custom',
 			get_bloginfo('template_directory') . '/js/custom.js',
-			array('jquery'), '1.03' , true );
+			array('jquery'), '1.04' , true );
 		$vars = array(
 			'url' => admin_url( 'admin-ajax.php' ),
 			'postid'=>get_the_ID()
