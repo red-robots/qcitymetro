a9a50aef26a0be01836114af9c0e8413a576f587
fritzhealy <fre.healy@gmail.com>
2018-06-06 12:27:34 -0400

    adding two column description layout

---
+++ page-templates/two-column-description.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Two Column Description
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="">
+		<div id="content" role="main" class="wrapper">
+
+			<?php while ( have_posts() ) : the_post(); 
+			$top = get_field("top_content");
+			$right = get_field('right_content');
+			?>
+            
+            <header class="archive-header">
+				<div class="border-title">
+                    <h1><?php the_title(); ?></h1>
+                </div><!-- border title -->
+				</header><!-- .archive-header -->
+                <div class="entry-content">
+					<?php echo $top;?>
+				</div><!--.entry-content-->
+                <div class="column-left">
+                	<div class="entry-content">
+                    	<?php the_content(); ?>
+                	</div><!-- entry-content -->
+                </div><!-- column left -->
+                
+                <div class="column-right">
+                	<div class="entry-content">
+                    	<?php echo $right; ?>
+                	</div><!-- entry-content -->
+                </div><!-- column left -->
+                
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
